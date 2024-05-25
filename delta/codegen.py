f8b8c8f2a8deb1c50da488d181258371ebd78db2
Eduardo Cortez Valente <130951926+EduardoCorVal@users.noreply.github.com>
2024-05-24 18:02:22 -0600

    S9. If/else statement

--- delta/codegen.py
+++ delta/codegen.py
@@ -38,6 +38,20 @@ class CodeGenerationVisitor(PTNodeVisitor):
     def visit_lhs_variable(self, node, children):
         name = node.value
         return f'    local.set ${name}\n'
+    
+    def visit_if(self, node, children):
+        result = (children[0]
+                  + '    if\n'
+                  + children[1])
+        if len(children) == 3:
+            result += ('    else\n'
+                       + children[2])
+        result += '    end\n'
+        return result
+
+    def visit_block(self, node, children):
+        return ''.join(children)
+
 
     def visit_expression(self, node, children):
         # print('expression', children)
